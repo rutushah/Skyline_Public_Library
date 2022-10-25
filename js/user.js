8e7a0d1428f09c1acca44bde8c096a3e21b413b6
unnati098 <unnatipatel098@gmail.com>
2022-10-25 10:35:20 -0400

    Added Login page and select role design.

---
+++ js/user.js
@@ -0,0 +1,14 @@
+/**
+ * Validation To check the user Role Selection On Select Role.php
+ * 
+ * Description : If user Role is not selected either admin, staff or user, user should get validation message to select role
+ * */ 
+ function selectRole(){
+    var select_role  = document.selectRole_form.role;
+    for(i=0; i<select_role.length; i++){
+        if(select_role[i].checked == true)
+            return true;
+    }
+    document.getElementById("err_msg").innerHTML = '<p style = "color:red">Please select the Role!!</p>';
+    return false;
+}
